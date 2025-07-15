e285dcb83acfdf92920538be183892395be4fe46
ManiRaja-DEV <hellomaniraja@gmail.com>
2025-07-15 13:34:18 +0530

    chore(config): add passthrough copy for images

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ export default async function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addWatchTarget("./src/js");
 
+  eleventyConfig.addPassthroughCopy("./src/images/");
+
   return {
     dir: {
       input: "src",
